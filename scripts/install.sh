dc92e1b76de63d409bede7924b7f1babc52fc15c
Cris Mihalache <cris.mihalache@tradeshift.com>
2022-04-01 05:41:41 +0300

    feature: basic install script & repo path fix

---
+++ scripts/install.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+git submodule init
+git submodule update
+
+if [[ -d "$HOME/.nvm" ]]; then
+  nvm use
+  yarn
+fi
+
+pip install --user dotfiles
+
+echo 'Point the dotfiles repo to the correct location in the next step.'
+echo 'Press Enter to continue...'
+read -r -n 1
+
+LOCAL_SOURCE_PATH="$(dirname "$0")"
+RC_PATH="$LOCAL_SOURCE_PATH/../dotfilesrc"
+
+"$EDITOR" "$RC_PATH"
+cp "$RC_PATH" "$HOME/.dotfilesrc"
+
+dotfiles -f -s
+
+pip install --user dotfiles neovim
